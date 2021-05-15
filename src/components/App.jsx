9b836d2e0111eba260a4d901e9c25294c01eac99
Victor Adeleke AFOLAYAN <adelekeafolayan@gmail.com>
2021-05-15 03:46:27 +0100

    implemented props and mapping to existing project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../Notes";
+
+// function createCard(notes) {
+//     return (
+//         <Note key={notes.key} title={notes.title} content={notes.content} />
+//     )
+// }
+
+// function App() {
+//     return (
+//         <div>
+//             <Header />
+//             {notes.map(createCard)}
+//             <Footer />
+//         </div>
+//     );
+// }
+
+// the above is the complete code. But to use arrow functions, let's work on it.
+
 
 function App() {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(notes => (
+        <Note key={notes.key} title={notes.title} content={notes.content} />
+    ))}
             <Footer />
         </div>
     );
